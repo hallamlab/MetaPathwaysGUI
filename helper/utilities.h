e5bb15e96c0ff427c970c9335e607356852df366
kishori <kishori82@gmail.com>
2015-01-04 18:40:49 -0800

    Updated the constants like TEMPLATE_FIL

--- helper/utilities.h
+++ helper/utilities.h
@@ -43,7 +43,7 @@ public:
     static QHash<QString,QString> parseFile(const QString &TEMPLATE_FILE, const QString TYPE);
     static QHash<QString,QString> parseFileConfig(const QString &CONFIG_FILE);
     static QString extractAttribute(QString line);
-    static bool writeSettingToFile(const QString &TEMPLATE_FILE, const QString TYPE, const QString &KEY, const QString &VALUE, const bool &CREATE, const bool &DELETE);
+
     static int countRunSteps(QHash<QString,QString>* PARAMS);
     static QStringList getUniqueDBS(QStringList dbs);
     static QStringList getSplitNames(QString nameString);
@@ -51,10 +51,15 @@ public:
     static QLabel *ShowWaitScreen(QString pngFile, QString msg ) ;
     static QLabel *ShowWaitScreen(QString msg) ;
     static bool checkEmptyFile(QString filepath);
+
+
     static QString insertCharacterAtIntervals(QString string, QChar c, int n);
     static QString getShortORFId(const QString &orfname);
     static QString getShortContigId(const QString &orfname);
-    static QString prepareToPrint(QString str, QChar delim);
+    static bool writeSettingToFile(const QString &TEMPLATE_FIL, const QString TYPE, const QString &KEY, const QString &VALUE, const bool &CREATE, const bool &DELET);
+
+
+   static QString prepareToPrint(QString str, QChar delim);
     static QStringList getFilesWithPattern(const QString &folderName, const QRegExp &pattern);
     static unsigned int numNonZeros(const QVector<double> &v);
     static void removeZeros(QVector<double> &x, QVector<double> &y, unsigned int index = 0 );
